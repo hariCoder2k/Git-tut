373beb02ab19cfd7f2392f75a5ab90f1701d27be
Hariharakrishnan <hariharakrishnan2002@gmail.com>
2022-07-05 17:48:21 +0530

    learning sorting and searching references :geekforgeeks,programiz will be further updated!

---
+++ Search/BinarySearch.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include"mergesort1.h"
+using namespace std;
+
+int BinarySearchRecursive(int array[] , int target ,int left,int right)
+{
+	if(left>right){
+		return -1;
+	}
+	
+	int mid  = left + ((right-left )/2);
+	if(array[mid] == target){
+		return mid;
+	}
+	else if(target < array[mid])
+	{
+		return BinarySearchRecursive(array,target,left,mid-1);
+	}
+	else{
+		return BinarySearchRecursive(array,target,mid+1,right);
+	}
+	
+}
+
+int main()
+{
+	int *arr,length,target;
+	cout<<"Enter size:";
+	cin>>length;
+	arr = new int[length];
+	for(int i=0;i<length;i++){
+		cout<<"Enter value:";
+		cin>>arr[i];
+	}
+	cout<<"Enter target to be found:";
+	cin>>target;
+	cout<<BinarySearchRecursive(arr,target,0,length-1);
+} 
